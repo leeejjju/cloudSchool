d088c4852cf4cf7d92b2c6d1e7d2c9f8ff745550
leeejjju <leeejjju@gmail.com>
2025-05-28 17:42:09 +0900

    2025-05-28

---
+++ Shell/parameter.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "This shell script name : $0" 
+echo "I can speak $1 and $2"                                                     
+echo "This shell script parameters are $*"
+echo "This shell script parameters are $@"
+echo "This parameter count is $#"
